f29b0255cf4ece39b5d448145673322ef2cc1012
Anonymous-Humanoid <68610963+Anonymous-Humanoid@users.noreply.github.com>
2024-10-15 23:40:03 -0400

    Improved formatting

--- utils/build.js
+++ utils/build.js
@@ -3,25 +3,25 @@ process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
 process.env.ASSET_PATH = '/';
 
-var webpack = require('webpack'),
-  path = require('path'),
-  fs = require('fs'),
-  config = require('../webpack.config'),
-  ZipPlugin = require('zip-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs');
+const config = require('../webpack.config');
+const ZipPlugin = require('zip-webpack-plugin');
 
 delete config.chromeExtensionBoilerplate;
 
 config.mode = 'production';
 
-var packageInfo = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
+let packageInfo = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
 
 config.plugins = (config.plugins || []).concat(
-  new ZipPlugin({
-    filename: `${packageInfo.name}-${packageInfo.version}.zip`,
-    path: path.join(__dirname, '../', 'zip'),
-  })
+    new ZipPlugin({
+        filename: `${packageInfo.name}-${packageInfo.version}.zip`,
+        path: path.join(__dirname, '../', 'zip'),
+    })
 );
 
 webpack(config, function (err) {
-  if (err) throw err;
+    if (err) throw err;
 });
