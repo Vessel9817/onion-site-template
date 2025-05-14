8afa72edbbc3d00344a905853ea369a966c53705
Vessel9817 <151808241+Vessel9817@users.noreply.github.com>
2025-05-13 21:23:39 -0400

    Renamed adapter to proxy and fixed HTTP proxy

--- config/onionscan/adapter/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Running nginx in background
-nginx -g 'daemon off;'
-
-# Running socat
-'/usr/bin/socat' TCP-LISTEN:9150,fork SOCKS5:10.5.4.2:9050,socksport=9150
---
+++ config/onionscan/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+'./onionscan' --verbose -webport=8080 --torProxyAddress=10.5.4.253 $HOSTNAME
