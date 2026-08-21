932da25cc28a57a6ad10193cdbb9089161eeef3d
Vessel9817 <151808241+Vessel9817@users.noreply.github.com>
2026-08-21 01:50:55 -0400

    chore: fix typo
    Signed-off-by: Vessel9817 <151808241+Vessel9817@users.noreply.github.com>

--- src/express/app.ts
+++ src/express/app.ts
@@ -15,7 +15,7 @@ app.disable('x-powered-by');
 // Setting global app middleware
 app.use(express.json()); // Parse Content-Type: json
 app.use(express.urlencoded({ extended: false })); // Encodes special characters in URLs
-app.use(csp); // Restrictions application permissions
+app.use(csp); // Restricts application permissions
 app.use('/', appRouter); // Serves app
 app.use('/', notFoundRouter); // Catches errors
 
