48def887fafefa0a6230e539115e7623f6c7838c
Vessel9817 <151808241+Vessel9817@users.noreply.github.com>
2026-08-21 00:24:12 -0400

    fix: enforce same-origin policy (#275)
    Fixes #275
    
    Signed-off-by: Vessel9817 <151808241+Vessel9817@users.noreply.github.com>

--- src/express/app.ts
+++ src/express/app.ts
@@ -1,7 +1,6 @@
-import cors from 'cors';
 import express from 'express';
 import mongoose from 'mongoose';
-import { domain, msgBoard } from './env';
+import { msgBoard } from './env';
 import { appRouter, notFoundRouter } from './routes';
 
 const app = express();
@@ -13,7 +12,6 @@ app.set('view engine', 'ejs');
 app.disable('x-powered-by');
 
 // Setting global app middleware
-app.use(cors({ origin: domain })); // Sets CORS policy
 app.use(express.json()); // Parse Content-Type: json
 app.use(express.urlencoded({ extended: false })); // Encodes special characters in URLs
 app.use('/', appRouter); // Serves app
