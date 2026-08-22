ba5de94614ce56519703fadd7a991f14aac6e52d
Vessel9817 <151808241+Vessel9817@users.noreply.github.com>
2026-08-22 18:37:16 -0400

    feat: disable sending ETag headers
    Completes a task of #284
    
    [Reference](https://github.com/expressjs/express/commit/610e172fcf9306bd5812bb2bae8904c23e0e8043)
    
    Signed-off-by: Vessel9817 <151808241+Vessel9817@users.noreply.github.com>

--- src/express/app.ts
+++ src/express/app.ts
@@ -11,6 +11,7 @@ app.set('view engine', 'ejs');
 
 // Removing fingerprintable headers
 app.disable('x-powered-by');
+app.disable('etag');
 
 // Setting global app middleware
 app.use(express.json()); // Parse Content-Type: json
