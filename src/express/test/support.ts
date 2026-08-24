7722ccc9615f638fcef2481a3173ca39f4b0f4dd
Vessel9817 <151808241+Vessel9817@users.noreply.github.com>
2026-08-24 16:16:35 -0400

    chore: minor simplifications
    Signed-off-by: Vessel9817 <151808241+Vessel9817@users.noreply.github.com>

--- src/express/test/support.ts
+++ src/express/test/support.ts
@@ -4,5 +4,5 @@ import { MsgModel } from '../db/msgBoard';
 export function stubAggregate(t: TestContext, docs: unknown[]) {
     return t.mock.method(MsgModel, 'aggregate', () => ({
         exec: () => Promise.resolve(docs)
-    }) as unknown as ReturnType<typeof MsgModel.aggregate>);
+    }) as ReturnType<typeof MsgModel.aggregate>);
 }
